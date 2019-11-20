1d7a54d4f90d3808864c8a10be3f3aee6b80725b
Jonathan Horvath <byteme@bytedreamer.com>
2019-11-19 21:56:07 -0500

    Add Nuget package info

--- src/OSDP.Net/Connections/IOsdpConnection.cs
+++ src/OSDP.Net/Connections/IOsdpConnection.cs
@@ -5,38 +5,38 @@ namespace OSDP.Net.Connections
 {
     public interface IOsdpConnection
     {
-     /// <summary>
+        /// <summary>
         /// Speed of the connection
         /// </summary>
         int BaudRate { get; }
 
-     /// <summary>
-         /// Is the connection open and ready to communicate
-         /// </summary>
+        /// <summary>
+        /// Is the connection open and ready to communicate
+        /// </summary>
         bool IsOpen { get; }
 
-     /// <summary>
-         /// Open the connection for communications
-         /// </summary>
+        /// <summary>
+        /// Open the connection for communications
+        /// </summary>
         void Open();
 
-     /// <summary>
-         /// Close the connection for communications
-         /// </summary>
+        /// <summary>
+        /// Close the connection for communications
+        /// </summary>
         void Close();
 
-     /// <summary>
-         /// Write to connection
-         /// </summary>
-         /// <param name="buffer">Array of bytes to write</param>
+        /// <summary>
+        /// Write to connection
+        /// </summary>
+        /// <param name="buffer">Array of bytes to write</param>
         Task WriteAsync(byte[] buffer);
 
-     /// <summary>
-         /// Read from connection
-         /// </summary>
-         /// <param name="buffer">Array of bytes to read</param>
-         /// <param name="token">Cancellation token to end reading of bytes</param>
-         /// <returns>Number of actual bytes read</returns>
-         Task<int> ReadAsync(byte[] buffer, CancellationToken token);
+        /// <summary>
+        /// Read from connection
+        /// </summary>
+        /// <param name="buffer">Array of bytes to read</param>
+        /// <param name="token">Cancellation token to end reading of bytes</param>
+        /// <returns>Number of actual bytes read</returns>
+        Task<int> ReadAsync(byte[] buffer, CancellationToken token);
     }
 }
