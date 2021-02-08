7048011fa55637f7fccf16f845181b7e6e096a92
Jonathan Horvath <byteme@bytedreamer.com>
2021-02-08 17:13:53 -0500

    Add Fingerprint definition to PIV Data

--- src/OSDP.Net/Model/CommandData/GetPIVData.cs
+++ src/OSDP.Net/Model/CommandData/GetPIVData.cs
@@ -24,6 +24,7 @@ namespace OSDP.Net.Model.CommandData
                 ObjectId.CardholderUniqueIdentifier => new byte[] {0x5F, 0xC1, 0x02, ElementId, DataOffset},
                 ObjectId.CertificateForPIVAuthentication => new byte[] {0x5F, 0xC1, 0x05, ElementId, DataOffset},
                 ObjectId.CertificateForCardAuthentication => new byte[] {0xDF, 0xC1, 0x01, ElementId, DataOffset},
+                ObjectId.CardholderFingerprintTemplate => new byte[] { 0xDF, 0xC1, 0x03, ElementId, DataOffset },
                 _ => throw new ArgumentOutOfRangeException()
             };
         }
@@ -33,6 +34,7 @@ namespace OSDP.Net.Model.CommandData
     {
         CardholderUniqueIdentifier,
         CertificateForPIVAuthentication,
-        CertificateForCardAuthentication
+        CertificateForCardAuthentication,
+        CardholderFingerprintTemplate
     }
 }
