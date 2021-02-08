04be47d0d5bdd811e8e2345c36bf8acb251692c4
Jonathan Horvath <byteme@bytedreamer.com>
2021-02-08 07:42:00 -0500

    Read buffer length is at expected size.

--- samples/SmartCardSample/Program.cs
+++ samples/SmartCardSample/Program.cs
@@ -158,10 +158,9 @@ namespace SmartCardSample
             }
 
             return builder.ToString();
-            
         }
 
-        public static byte[] StringToByteArray(string hex) {
+        private static byte[] StringToByteArray(string hex) {
             return Enumerable.Range(0, hex.Length)
                 .Where(x => x % 2 == 0)
                 .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
