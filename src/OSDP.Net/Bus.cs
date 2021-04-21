24df17d229021d28414457a9760240e6b25e022b
Jonathan Horvath <byteme@bytedreamer.com>
2021-04-20 20:57:10 -0400

    Cleanup code

--- src/OSDP.Net/Bus.cs
+++ src/OSDP.Net/Bus.cs
@@ -3,7 +3,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
