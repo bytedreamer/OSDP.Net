b4cf1736cafab8f07a2e84f2527903138b8022c8
Dennis Mnuskin <dennis.mnuskin@gmail.com>
2022-10-27 15:58:49 -0400

    WIP: First draft at introducing exception on NAK reply

---
+++ src/OSDP.Net/Exceptions.cs
@@ -0,0 +1,36 @@
+﻿using OSDP.Net.Messages;
+using OSDP.Net.Model.ReplyData;
+using System;
+
+namespace OSDP.Net
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class OSDPNetException : Exception { }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CommandException : OSDPNetException { }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class NackReplyException : CommandException
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public Nak Reply { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="replyData"></param>
+        public NackReplyException(Nak replyData) 
+        {
+            Reply = replyData;
+        }
+    }
+}
