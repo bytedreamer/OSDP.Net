754e0cb61842f6cc63e19848ae8daf6357e3c944
Jonathan Horvath <byteme@bytedreamer.com>
2024-01-04 14:27:10 -0500

    #142 Add multi-part manufacture commands and fix existing authentication challenge command

--- src/OSDP.Net/Model/CommandData/MessageDataFragment.cs
+++ src/OSDP.Net/Model/CommandData/MessageDataFragment.cs
@@ -5,18 +5,18 @@ using OSDP.Net.Messages;
 namespace OSDP.Net.Model.CommandData
 {
     /// <summary>
-    /// Command to instruct PD to perform a cryptographic challenge/response sequence
+    /// Represents a fragment of data for a multi-part message.
     /// </summary>
-    internal class AuthenticationChallengeFragment
+    internal class MessageDataFragment
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="FileTransferFragment"/> class.
+        /// Initializes a new instance of the <see cref="MessageDataFragment"/> class.
         /// </summary>
         /// <param name="totalSize">File size as little-endian format</param>
         /// <param name="offset">Offset of the current message</param>
         /// <param name="fragmentSize">Size of the fragment</param>
         /// <param name="dataFragment">Authentication challenge fragment data</param>
-        public AuthenticationChallengeFragment(int totalSize, int offset, ushort fragmentSize, byte[] dataFragment)
+        public MessageDataFragment(ushort totalSize, ushort offset, ushort fragmentSize, byte[] dataFragment)
         {
             TotalSize = totalSize;
             Offset = offset;
@@ -25,14 +25,14 @@ namespace OSDP.Net.Model.CommandData
         }
 
         /// <summary>
-        /// Get the file size as little-endian format
+        /// Get the total message data size as little-endian format
         /// </summary>
-        public int TotalSize { get; }
+        public ushort TotalSize { get; }
 
         /// <summary>
         /// Get the offset of the current message
         /// </summary>
-        public int Offset { get; }
+        public ushort Offset { get; }
 
         /// <summary>
         /// Get the size of the fragment
@@ -40,15 +40,15 @@ namespace OSDP.Net.Model.CommandData
         public ushort FragmentSize { get; }
 
         /// <summary>
-        /// Get the authentication challenge fragment data
+        /// Get the fragment data
         /// </summary>
         public byte[] DataFragment { get; }
 
         internal ReadOnlySpan<byte> BuildData()
         {
             var data = new List<byte>();
-            data.AddRange(Message.ConvertIntToBytes(TotalSize));
-            data.AddRange(Message.ConvertIntToBytes(Offset));
+            data.AddRange(Message.ConvertShortToBytes(TotalSize));
+            data.AddRange(Message.ConvertShortToBytes(Offset));
             data.AddRange(Message.ConvertShortToBytes(FragmentSize));
             data.AddRange(DataFragment);
             return data.ToArray();
@@ -56,12 +56,12 @@ namespace OSDP.Net.Model.CommandData
 
         /// <summary>Parses the message payload bytes</summary>
         /// <param name="data">Message payload as bytes</param>
-        /// <returns>An instance of FileTransferFragment representing the message payload</returns>
-        public static AuthenticationChallengeFragment ParseData(ReadOnlySpan<byte> data)
+        /// <returns>An instance of MessageDataFragment representing the message payload</returns>
+        public static MessageDataFragment ParseData(ReadOnlySpan<byte> data)
         {
-            return new AuthenticationChallengeFragment(
-                Message.ConvertBytesToInt(data.Slice(0, 4).ToArray()),
-                Message.ConvertBytesToInt(data.Slice(4, 4).ToArray()),
+            return new MessageDataFragment(
+                Message.ConvertBytesToUnsignedShort(data.Slice(0, 4).ToArray()),
+                Message.ConvertBytesToUnsignedShort(data.Slice(4, 4).ToArray()),
                 Message.ConvertBytesToUnsignedShort(data.Slice(8, 2).ToArray()),
                 data.Slice(10).ToArray());
         }
